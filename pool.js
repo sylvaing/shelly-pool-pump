61d8320578cd207dfcd65fa7160043f690339ac6
Guillaume Duveau <guillaume.duveau@gmail.com>
2024-05-14 23:43:16 +0200

    Fix subscribe to events.

--- pool.js
+++ pool.js
@@ -928,72 +928,73 @@ function update_temp_call(){
 
 }
 
-
-Shelly.addEventHandler(
-  function (data) {
-
-    let re = JSON.stringify(data);
-    print("EVENT", re);
-
-
-    /**
-    * Publish update on switch change on binary sensor
-    */
-    if (
-      data.component === "switch:0" &&
-      typeof data.info.output !== "undefined"
-    ) {
-      let _state_str = data.info.output ? "ON" : "OFF";
-      MQTT.publish(buildMQTTStateCmdTopics("binary_sensor", "state"), _state_str);
+function subscribe_to_events() {
+  Shelly.addEventHandler(
+    function (data) {
+  
+      let re = JSON.stringify(data);
+      print("EVENT", re);
+  
+  
+      /**
+      * Publish update on switch change on binary sensor
+      */
+      if (
+        data.component === "switch:0" &&
+        typeof data.info.output !== "undefined"
+      ) {
+        let _state_str = data.info.output ? "ON" : "OFF";
+        MQTT.publish(buildMQTTStateCmdTopics("binary_sensor", "state"), _state_str);
+    
+      }
+  
+      // Event pour changement de température
+      //let re = JSON.stringify(data);
+      //print("EVENTTTTTT", re);
+      if (data.info.event === "temperature_change") {
+        if (data.info.id === CONFIG.shelly_id_temp_ext) {
+          // changement de la temperature exterieur
+          STATUS.temp_ext = data.info.tC;
+          print("changement de la temperature exterieur");
+        }
+        if (data.info.id === CONFIG.shelly_id_temp_pool) {
+          // changement de la temperature pool
+          STATUS.current_temp = data.info.tC;
+          print("changement de la temperature piscine");
+        }
+        //process mise à jour des temperature
+        update_temp(false,false);
+        publishState()
+      }
+  
+      //event changement du switch et lock
+      if (data.info.event === "toggle") {
+  
+        let result = Shelly.getComponentStatus("switch",0); 
+        print("[POOL_] TOGGLE EVENT GETCOMPONENT-STATUS SWITCH :", result.output);
+  
+        let _state_str = result.output ? "ON" : "OFF";
+        MQTT.publish(buildMQTTStateCmdTopics("binary_sensor", "state"), _state_str);
+  
+        STATUS.tick_lock++;
+        print("[POOL] disable temp", STATUS.tick_lock);
+  
+        if (STATUS.disable_temp !== null)
+          Timer.clear(STATUS.disable_temp);
+  
+        STATUS.disable_temp = Timer.set(
+          600 * 1000,
+          false,
+          function () {
+            print("[POOL] re-enable temp");
+            STATUS.disable_temp = null;
+          }
+        );
+      }
   
     }
-
-    // Event pour changement de température
-    //let re = JSON.stringify(data);
-    //print("EVENTTTTTT", re);
-    if (data.info.event === "temperature_change") {
-      if (data.info.id === CONFIG.shelly_id_temp_ext) {
-        // changement de la temperature exterieur
-        STATUS.temp_ext = data.info.tC;
-        print("changement de la temperature exterieur");
-      }
-      if (data.info.id === CONFIG.shelly_id_temp_pool) {
-        // changement de la temperature pool
-        STATUS.current_temp = data.info.tC;
-        print("changement de la temperature piscine");
-      }
-      //process mise à jour des temperature
-      update_temp(false,false);
-      publishState()
-    }
-
-    //event changement du switch et lock
-    if (data.info.event === "toggle") {
-
-      let result = Shelly.getComponentStatus("switch",0); 
-      print("[POOL_] TOGGLE EVENT GETCOMPONENT-STATUS SWITCH :", result.output);
-
-      let _state_str = result.output ? "ON" : "OFF";
-      MQTT.publish(buildMQTTStateCmdTopics("binary_sensor", "state"), _state_str);
-
-      STATUS.tick_lock++;
-      print("[POOL] disable temp", STATUS.tick_lock);
-
-      if (STATUS.disable_temp !== null)
-        Timer.clear(STATUS.disable_temp);
-
-      STATUS.disable_temp = Timer.set(
-        600 * 1000,
-        false,
-        function () {
-          print("[POOL] re-enable temp");
-          STATUS.disable_temp = null;
-        }
-      );
-    }
-
-  }
-);
+  );
+}
 
 /**
  * Main.
@@ -1020,6 +1021,9 @@ Timer.set(10000, false, function () {
 
    // Activate periodic updates
   if(CONFIG.update_period > 0) Timer.set(CONFIG.update_period, true, publishState);
+
+  // Susbcribe to events.
+  subscribe_to_events();
 
   // Activate periodic check for new day
   // 300000 = 5min
